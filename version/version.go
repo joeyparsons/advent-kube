3696ef3990bd81ba506b5f7598f967eeae8848f3
Joey Parsons <joey.parsonse@gmail.com>
2017-12-29 02:37:53 +0000

    [version] add versioning

---
+++ version/version.go
@@ -0,0 +1,10 @@
+package version
+
+var (
+	// BuildTime is a time label of the moment the binary was built
+	BuildTime = "unset"
+	// Commit is a last commit hash at the moment the binary was built
+	Commit = "unset"
+	// Release is a semantic version of the current build
+	Release = "unset"
+)
